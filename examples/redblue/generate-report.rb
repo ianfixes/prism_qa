989aa41c8bf9d1d0061f34264b6b59e893d6cf3d
Ian Katz <iakatz@paypal.com>
2015-05-22 11:27:38 -0400

    [examples] Comments and clarity added

--- examples/redblue/generate-report.rb
+++ examples/redblue/generate-report.rb
@@ -1,7 +1,9 @@
 require 'bundler/setup'
 require 'vivisector'
 
+# Minimal anatomy example: we hard-code 3 images we know are in the filesystem
 class TestAnatomy < Vivisector::Anatomy
+
   def fetch_image_set
     dis = Vivisector::DesignImageSet.new
 
@@ -27,9 +29,12 @@ class TestAnatomy < Vivisector::Anatomy
 
     dis
   end
+
 end
 
 
+# Minimal appography example. We expect to have several of these.
+# We hard-code the target here, and use path_component to indicate its attribute
 class TestAppography < Vivisector::Appography
   attr_accessor :path_component
 
@@ -40,13 +45,13 @@ class TestAppography < Vivisector::Appography
     ais.target.name = "#{@path_component} device"
     ais.target.attribute = @path_component
 
-    im1 = Vivisector::Image.new
+    im1 = Vivisector::AppImage.new
     im1.id = "r"
     im1.description = "red"
     im1.path = File.join(File.dirname(__FILE__), "input", @path_component, "red.png")
     ais.add(im1)
 
-    im2 = Vivisector::Image.new
+    im2 = Vivisector::AppImage.new
     im2.id = "b"
     im2.description = "blue"
     im2.path = File.join(File.dirname(__FILE__), "input", @path_component, "blue.png")
@@ -58,23 +63,30 @@ class TestAppography < Vivisector::Appography
 end
 
 
+# Create instances of the anatomy and appography(ies) we will use
 my_anatomy = TestAnatomy.new
+
 my_appography_short = TestAppography.new
+my_appography_short.path_component = "short" # also the attribute of this "device"
+
 my_appography_tall  = TestAppography.new
+my_appography_tall.path_component  = "tall"  # also the attribute of this "device"
 
-my_appography_short.path_component = "short"
-my_appography_tall.path_component  = "tall"
 
+# Create the function we will use to determine the report names
 def title_for_attribute attribute
   attribute = "default" if attribute.nil?
   "Report for #{attribute}"
 end
 
+# Create the function we will use to determine the report filenames
 def html_report_path_for_attribute attribute
   attribute = "default" if attribute.nil?
   File.join(File.dirname(__FILE__), "output", "#{attribute}.html")
 end
 
+
+# Kick off the reporting; the images will be fetched at this point.
 Vivisector::report(my_anatomy,
                    [my_appography_short, my_appography_tall],
                    method(:title_for_attribute),
